8d1cea073e0f1e72586cff410f6c0ef21502a1b6
qinmenghuo <qinmenghuo@gamil.com>
2021-03-14 17:01:50 +0800

    尝试性的添加了多页读取

--- main.py
+++ main.py
@@ -5,21 +5,25 @@ import requests
 #输入信息
 kw=input("请输入要搜索的tag：")
 url = 'https://e-hentai.org/'
-#搜索参数
-param = {
-    'f_cats':'1017',
-    'f_search': kw,
-}
 #UA伪装
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'
 }
-#获取数据
-response = requests.get(url=url, params=param, headers=headers)
+#搜索参数
+for page in range(1,6):
+    page=str(page)
+    param = {
+        'f_cats':'1017',
+        'f_search': kw,
+        'page':page,
+    }
 
-page_text=response.text
-filename=kw+'.html'
-with open(filename,'w',encoding='utf-8') as fp:
-    fp.write(page_text)
+    #获取数据
+    response = requests.get(url=url, params=param, headers=headers)
+
+    page_text=response.text
+    filename=kw+page+'.html'
+    with open(filename,'w',encoding='utf-8') as fp:
+        fp.write(page_text)
 
 print(filename+"保存成功")
