ed3308035fed727dba02339fbc2a88e1de142742
qinmenghuo <qinmenghuo@gamil.com>
2021-03-19 20:32:01 +0800

    可以稳定爬ex本了，请自行手动添加cookie和代理ip

--- download.py
+++ download.py
@@ -1,6 +1,6 @@
 import requests
 import re
-import setting
+import setting_f
 import os
 import time
 import random
@@ -17,13 +17,13 @@ def wenjianjia(topic_l):
 def qingqiu(url):
     """各种乱七八糟的请求"""
 
-    response_1 = requests.get(url=url, headers=setting.headers, verify=False).text  # 请求漫画页面的html
-    topic_l = re.findall(setting.topic_t, response_1, re.S)  # 请求漫画名（获得的是单元素列表，需要提取）
+    response_1 = requests.get(url=url, headers=setting_f.headers, verify=False).text  # 请求漫画页面的html
+    topic_l = re.findall(setting_f.topic_t, response_1, re.S)  # 请求漫画名（获得的是单元素列表，需要提取）
     topic = wenjianjia(topic_l)  # 提取漫画名并创建对应文件夹
-    html_list = re.findall(setting.ex, response_1, re.S)  # 获取漫画单页具体页面的url（列表）
+    html_list = re.findall(setting_f.ex, response_1, re.S)  # 获取漫画单页具体页面的url（列表）
     for list in html_list:  # 提取单页漫画url
-        img_html = requests.get(url=list, headers=setting.headers, verify=False).text  # 获取url对应漫画页html
-        img_data = re.findall(setting.exto, img_html, re.S)  # 获取图片url（单元素列表）
+        img_html = requests.get(url=list, headers=setting_f.headers, verify=False).text  # 获取url对应漫画页html
+        img_data = re.findall(setting_f.exto, img_html, re.S)  # 获取图片url（单元素列表）
         for data in img_data:  # 提取图片url
             download(data, topic)
 
@@ -31,7 +31,7 @@ def qingqiu(url):
 def download(data, topic):
     """下载图片"""
     time.sleep(random.uniform(1, 3))  # 随机休眠1-3秒
-    img_down = requests.get(url=data, headers=setting.headers, verify=False).content  # 二进制形式下载图片
+    img_down = requests.get(url=data, headers=setting_f.headers, verify=False).content  # 二进制形式下载图片
     img_name = data.split('/')[-1]  # 以图片名最后作为文件名
     imgPath = './' + topic + '/' + img_name  # 图片路径
     # 将图片数据写入文件
